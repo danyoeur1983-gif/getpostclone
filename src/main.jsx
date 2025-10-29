2eb79b0767ed2c7f24a8d1d3be7ca2109ab99bf4
danyoeur1983-gif <danyoeur1983@gmail.com>
2025-10-28 17:42:42 -0700

    Update main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
+```
+4. Commit changes
+```
+
+---
+
+#### **File 5: src/index.css** (Create if missing)
+```
+1. In src folder, create: index.css
+2. Paste:
