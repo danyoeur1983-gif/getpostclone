d3b5e4746c1b0b0d836a5e8474467ba5e021b956
danyoeur1983-gif <danyoeur1983@gmail.com>
2025-10-28 17:40:52 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
+```
+```
+4. Commit
+```
+
+---
+
+#### **File 3: tailwind.config.js** (Update if exists, or create)
+```
+1. Find or create: tailwind.config.js
+2. Replace with:
