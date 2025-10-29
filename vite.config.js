6999b3abd4bc2f5b03829aac0700bc2d4f4928f4
danyoeur1983-gif <danyoeur1983@gmail.com>
2025-10-28 17:58:42 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-```
-```
-4. Commit
-```
-
----
-
-#### **File 3: tailwind.config.js** (Update if exists, or create)
-```
-1. Find or create: tailwind.config.js
-2. Replace with:
